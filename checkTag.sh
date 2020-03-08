303e8e801ae26c036fd1400d966234111be3d727
mmagyar <mmagyar@instructure.com>
2020-03-08 17:58:09 +0100

    update workflow - auto tagging

--- checkTag.sh
+++ checkTag.sh
@@ -4,7 +4,9 @@ GIT_TAG=`git describe --exact-match --tags HEAD`
 
 
 if [ "$VERSION" != "$GIT_TAG" ]; then
-    echo "Current commit's tag ($GIT_TAG) does not match package version, add a git tag to this commit: $VERSION"
+    echo "Current commit's tag ($GIT_TAG) does not match package version, add a git tag to this commit: $VERSION\n"
+    echo "You may need to manually re-run this pipline after adding the tag\n"
+    echo "To add a tag run: git tag $VERSION && git push origin --tags"
 exit 1
 fi
 
