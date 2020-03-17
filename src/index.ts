bc52edd5f096305dbd2a3149401ccdf79c1a30f1
Magyar Máté <dev@mmagyar.com>
2020-03-17 21:47:35 +0100

    add type export

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { validate } from "./validate";
 export * from "./validate";
 export * from "./generate";
 export * from "./type";
+export * from "./validationTypes";
 
 export default validate;
