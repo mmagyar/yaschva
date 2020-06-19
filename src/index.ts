eb4bf50a33fe2e7691dc21c221ef63c55963ed02
Magyar Máté <dev@mmagyar.com>
2020-06-19 21:05:19 +0000

    update deps

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,4 @@ export * from './validate'
 export * from './generate'
 export * from './type'
 export * from './validationTypes'
-export * as typeSchema from '../typeSchema.json'
 export default validate
