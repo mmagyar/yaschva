27a7c1dbf41fb5015a755bbc6b1b5ca7a18af815
Magyar Máté <dev@mmagyar.com>
2020-08-11 00:25:29 +0200

    remove default export from index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-import { validate } from './validate.js'
-
 export * from './validate.js'
 export * from './generate.js'
 export * from './type.js'
 export * from './validationTypes.js'
-export default validate
